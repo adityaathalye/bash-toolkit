9cf1e9770ee928fba33a366c6a3bb229fd78dc41
Aditya Athalye <aditya.athalye@gmail.com>
2021-10-04 10:30:58 +0530

    Add custom package install for jami video conferencing

--- machine-setup.sh
+++ machine-setup.sh
@@ -48,6 +48,14 @@ __apt_install_custom_nextdns() {
     fi
 }
 
+__apt_install_custom_jami_p2p_videoconf() {
+    # https://jami.net/download-jami-linux/
+    sudo apt install gnupg dirmngr ca-certificates curl --no-install-recommends
+    curl -s https://dl.jami.net/public-key.gpg | sudo tee /usr/share/keyrings/jami-archive-keyring.gpg > /dev/null
+    sudo sh -c "echo 'deb [signed-by=/usr/share/keyrings/jami-archive-keyring.gpg] https://dl.jami.net/nightly/ubuntu_20.04/ ring main' > /etc/apt/sources.list.d/jami.list"
+    sudo apt-get update && sudo apt-get install jami
+}
+
 __dpkg_install_chrome() {
     if ! which google-chrome > /dev/null
     then curl https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb \
